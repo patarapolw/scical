79ef355d93d2aeda1a8e374cc035bd7715529302
patarapolw <patarapolw@gmail.com>
2018-03-11 12:35:57 +0700

    Apply Shunting-yard algorithm

--- scical.py
+++ scical.py
@@ -2,16 +2,20 @@ import re
 import itertools
 from operator import add, sub, mul, truediv, pow
 
+import logging
+
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+
 
 class Calculator:
     def __init__(self):
-        self.operator = {
-            '+': 'ADD',
-            '-': 'SUB',
-            '*': 'MUL',
-            '/': 'DIV',
-            '^': 'EXP',
-        }
+        self.operator = [
+            ('+', 'ADD', add),
+            ('-', 'SUB', sub),
+            ('*', 'MUL', mul),
+            ('/', 'DIV', truediv),
+            ('^', 'EXP', pow)
+        ]
 
         self.brackets = [
             ('(', ')'),
@@ -19,43 +23,7 @@ class Calculator:
             ('{', '}')
         ]
 
-        self._token_type = self.operator.copy()
-        self._token_type.update([(bracket, 'PAR') for bracket in itertools.chain(*self.brackets)])
-
-    def from_expr(self, expr):
-        split_expr = re.findall('[\d.]+|[{}]'.format(''.join(['\\'+token for token in self._token_type])), expr)
-        self._tokens = [(self._token_type.get(x, 'NUM'), x) for x in split_expr]
-        self._rpn = self._to_rpn()
-
-    def from_rpn(self, rpn_string):
-        rpn = rpn_string.split(' ')
-        self._rpn = [(self._token_type.get(value, 'NUM'), value) for value in rpn]
-
-    def result(self):
-        calc_map = {
-            'ADD': add,
-            'SUB': sub,
-            'MUL': mul,
-            'DIV': truediv,
-            'EXP': pow,
-        }
-
-        def operate_rpn(rpn):
-            if len(rpn) == 1:
-                return rpn[0]
-            for i, item in enumerate(rpn):
-                type, value = item
-                if type in self.operator.values():
-                    new_rpn = rpn[:i-2] \
-                              + [('NUM', calc_map[type](float(rpn[i-2][1]), float(rpn[i-1][1])))] \
-                              + rpn[i+1:]
-                    return operate_rpn(new_rpn)
-
-        return operate_rpn(self._rpn)[1]
-
-
-    def _to_rpn(self):
-        rule_sequence = [
+        self.rule_sequence = [
             ('X', ['EXP']),
             # B
             # O
@@ -64,10 +32,39 @@ class Calculator:
             ('AS', ['ADD', 'SUB']),
         ]
 
-        result = []
-        op_order = 0
+        self._token_type = dict((key, val) for key, val, op in self.operator)
+        self._token_type.update([(bracket, 'PAR') for bracket in itertools.chain(*self.brackets)])
+
+    def from_expr(self, expr):
+        split_expr = re.findall('[\d.]+|[{}]'.format(''.join(['\\'+token for token in self._token_type])), expr)
+        self._tokens = [(self._token_type.get(x, 'NUM'), x) for x in split_expr]
+        self.rpn = self._to_rpn()
+
+        return self.result()
+
+    def from_rpn(self, rpn_string):
+        rpn = re.findall('[\d.]+|[{}]'.format(''.join(['\\'+token for token in self._token_type])), rpn_string)
+        self.rpn = [(self._token_type.get(value, 'NUM'), value) for value in rpn]
+
+        return self.result()
+
+    def result(self):
+        def operate_rpn(rpn):
+            if len(rpn) == 1:
+                return rpn[0]
+            for i, item in enumerate(rpn):
+                type, value = item
+                for key, token, op in self.operator:
+                    if type == token:
+                        new_rpn = rpn[:i-2] \
+                                  + [('NUM', op(float(rpn[i-2][1]), float(rpn[i-1][1])))] \
+                                  + rpn[i+1:]
+                        return operate_rpn(new_rpn)
+
+        return operate_rpn(self.rpn)[1]
+
+    def _to_rpn(self):
         tokens = self._tokens
-        used_token = []
         all_brackets = list(itertools.chain(*self.brackets))
 
         def find_matching_brackets(input_bracket):
@@ -80,29 +77,70 @@ class Calculator:
                         to_find = bracket_pair[0]
                     break
 
-            for i, token in enumerate(tokens):
+            for token in tokens:
                 if token[1] == to_find:
-                    return i
-            return -1
+                    return token
+            return None
 
-        for rule in rule_sequence:
-            for i, token in enumerate(tokens):
-                if token in used_token:
-                    continue
-                if token[0] in rule[1]:
-                    result.append((op_order, token))
-                    used_token.append(token)
-                    op_order += 1
-                    adjacent = [i-1, i+1]
-                    for j in [i-1, i+1]:
-                        if tokens[j][1] in all_brackets:
-                            adjacent.append(find_matching_brackets(tokens[j][1]))
-                    for j in adjacent:
-                        result.append((op_order, tokens[j]))
-                        used_token.append(token)
-                    op_order += 1
+        num_stack = []
+        op_stack = [[]]
+        min_rule = None
+        bracket_level = 0
 
-        return [token for rule_order, token in sorted(result, reverse=True) if token[1] not in all_brackets]
+        def purge():
+            nonlocal op_stack
+
+            for op in op_stack[bracket_level][::-1]:
+                num_stack.append(op)
+            op_stack[bracket_level] = []
+
+        def purge_on_rule_violation(token):
+            logging.debug('Rule violation, purging...')
+            def get_rule_seq():
+                for i, item in enumerate(self.rule_sequence):
+                    if token[0] in item[1]:
+                        return i
+                return -1
+
+            nonlocal min_rule
+
+            if min_rule is not None:
+                if get_rule_seq() > min_rule:
+                    purge()
+                    min_rule = None
+                else:
+                    min_rule = get_rule_seq()
+            else:
+                min_rule = get_rule_seq()
+
+        pair_bracket = None
+
+        logging.debug('Bracket level: ' + str(bracket_level))
+        for token in tokens:
+            if pair_bracket == token:
+                purge()
+                pair_bracket = None
+                op_stack = op_stack[:-1]
+                bracket_level -= 1
+                logging.debug('Found closing bracket, purging...')
+                logging.debug('Op_stack:' + repr(op_stack))
+                logging.debug('Bracket level: ' + str(bracket_level))
+            elif token[0] == 'NUM':
+                num_stack.append(token)
+                logging.debug('Num_stack:' + repr(num_stack))
+            elif token[0] == 'PAR':
+                pair_bracket = find_matching_brackets(token[1])
+                op_stack.append([])
+                bracket_level += 1
+                logging.debug('Found opening bracket')
+                logging.debug('Bracket level: ' + str(bracket_level))
+            else:
+                purge_on_rule_violation(token)
+                op_stack[bracket_level].append(token)
+                logging.debug('Op_stack:' + repr(op_stack))
+        purge()
+
+        return num_stack
 
     def to_rpn(self):
         return [token[1] for token in self._to_rpn()]
@@ -110,18 +148,16 @@ class Calculator:
 
 def parse(expr):
     calculator = Calculator()
-    calculator.from_expr(expr)
-    return calculator.result()
+    return calculator.from_expr(expr)
 
 
 def parse_rpn(rpn_string):
     calculator = Calculator()
-    calculator.from_rpn(rpn_string)
-    return calculator.result()
+    return calculator.from_rpn(rpn_string)
 
 
 if __name__ == '__main__':
-    formula = '( 6 - 3) ^2 - 11'
-    print(parse(formula))
-    rpn_string = '162 2 1 + 4 ^ /'
-    print(parse_rpn(rpn_string))
+    formula = '162 / (2 + 1 ) ^4'
+    cal = Calculator()
+    cal.from_expr(formula)
+    print(cal.result())
